c760620dc8992a2bfba08fbba28e7efb6b311fb8
Koji Eguchi <koji.eguchi@hotmail.com>
2023-12-08 20:09:52 -0500

    Generate EmployerPage component

--- justTalent/src/app/app.component.ts
+++ justTalent/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { EmployerPageComponent } from './employer-page/employer-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, HomeComponent, NavbarComponent, EmployerPageComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ justTalent/src/app/employer-page/employer-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-page.component.html',
+  styleUrl: './employer-page.component.css'
+})
+export class EmployerPageComponent {
+
+}
