505ff71fae7ea32a2d970f22f9d4b0b654f8cad1
Koji Eguchi <koji.eguchi@hotmail.com>
2023-12-08 20:18:55 -0500

    Add RouterLink to Navbar component

--- justTalent/src/app/app.component.ts
+++ justTalent/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { EmployerPageComponent } from './employer-page/employer-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent, NavbarComponent, EmployerPageComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- justTalent/src/app/app.routes.ts
+++ justTalent/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployerPageComponent } from './employer-page/employer-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {'path':'', component:HomeComponent},
+  {'path':'employer-page', component:EmployerPageComponent}
+];
