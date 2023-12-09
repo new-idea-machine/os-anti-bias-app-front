933da30564d0004536d2e53b5e3aeb916593028f
Koji Eguchi <koji.eguchi@hotmail.com>
2023-12-08 20:05:28 -0500

    Display Navbar and Home component on application

--- justTalent/src/app/app.component.ts
+++ justTalent/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HomeComponent, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
