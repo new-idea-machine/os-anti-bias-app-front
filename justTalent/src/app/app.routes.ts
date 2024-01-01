1b156c0983d8c81d754c1a522e8526b3a95188b0
Graydon Ritchiee <graydonritchie@gmail.com>
2024-01-01 13:52:30 -0500

    adds candidate search componenet

--- justTalent/src/app/app.routes.ts
+++ justTalent/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmployerPageComponent } from './employer-page/employer-page.component';
+import { CandidateSearchComponent } from './candidate-search/candidate-search.component';
 
 export const routes: Routes = [
   {'path':'', component:HomeComponent},
-  {'path':'employer-page', component:EmployerPageComponent}
+  {'path':'employer-page', component:EmployerPageComponent},
+  {'path':'candidate-search', component:CandidateSearchComponent}
 ];
---
+++ justTalent/src/app/candidate-search/candidate-search.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import candidates from "../data/candidateData.json"
+
+@Component({
+  selector: 'app-candidate-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './candidate-search.component.html',
+  styleUrl: './candidate-search.component.css'
+})
+export class CandidateSearchComponent {
+
+}
