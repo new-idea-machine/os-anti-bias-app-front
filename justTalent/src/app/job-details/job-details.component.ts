eb66b45e63e2f0ccebbd63819a02a5a6e0d4489c
Koji Eguchi <koji.eguchi@hotmail.com>
2024-01-06 11:26:47 -0500

    Generate Job Details Component

---
+++ justTalent/src/app/job-details/job-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-details.component.html',
+  styleUrl: './job-details.component.css'
+})
+export class JobDetailsComponent {
+
+}
