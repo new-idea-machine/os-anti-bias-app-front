647c3ebd75018a654caaf0a15ed9326963adcda8
Koji Eguchi <koji.eguchi@hotmail.com>
2024-01-06 12:10:46 -0500

    Use routerLink to direct Job Details page from Job Posting List

--- justTalent/src/app/job-list/job-list-item/job-list-item.component.ts
+++ justTalent/src/app/job-list/job-list-item/job-list-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { JobPost } from '../../interfaces/job-post';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-job-list-item',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './job-list-item.component.html',
   styleUrl: './job-list-item.component.css'
 })
