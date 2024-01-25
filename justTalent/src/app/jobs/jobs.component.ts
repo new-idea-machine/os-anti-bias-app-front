9f0e7ef4edafa1c7b62f125413d44366bf48821a
Graydon Ritchiee <graydonritchie@gmail.com>
2024-01-24 19:18:59 -0500

    adds to home + duplicates candidate search for job search

--- justTalent/src/app/jobs/jobs.component.ts
+++ justTalent/src/app/jobs/jobs.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import candidateData from "../../data/candidateData.json"
 
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
 export class JobsComponent {
-
+  candidates = candidateData
 }
