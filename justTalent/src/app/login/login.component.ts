3f4970ca061706f399b16caa5838a617e97bef75
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-10 15:55:52 -0500

    Create a base form

--- justTalent/src/app/login/login.component.ts
+++ justTalent/src/app/login/login.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    console.log('login')
+  }
 }
