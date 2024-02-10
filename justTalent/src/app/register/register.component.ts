d103c2fd7627b4cf9e1cc2de96e982a537103d5f
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-10 16:37:48 -0500

    Import ProvideHttpClient to app.config.ts and create onSubmit logic for Register page

--- justTalent/src/app/register/register.component.ts
+++ justTalent/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../interfaces/user';
 
 
 @Component({
@@ -11,6 +13,8 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   fb = inject(FormBuilder);
+  // Angular 17 - ensure to import ProvideHttpClient in app.config.ts file
+  http = inject(HttpClient);
 
   form = this.fb.nonNullable.group({
     username: ['', Validators.required],
@@ -19,6 +23,13 @@ export class RegisterComponent {
   });
 
   onSubmit(): void{
-    console.log('register');
+    this.http.post< { user: User }>(
+      'https://api.realworld.io/api/users',
+      {
+        user: this.form.getRawValue(),
+      }
+    ).subscribe(response => {
+      console.log('response',response)
+    })
   }
 }
