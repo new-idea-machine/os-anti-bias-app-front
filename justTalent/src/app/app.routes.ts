d9f34504d8f24b4356f0de2f0e7d1db47b667ee0
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-10 15:48:20 -0500

    Add a route for Login page

--- justTalent/src/app/app.routes.ts
+++ justTalent/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { EmployerPageComponent } from './employer-page/employer-page.component';
 import { JobDetailsComponent } from './job-details/job-details.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {'path':'', component:HomeComponent},
   {'path':'employer-page/:id', component:EmployerPageComponent},
   {'path':'job-details/:id', component:JobDetailsComponent},
-  {'path':'register', component:RegisterComponent}
+  {'path':'register', component:RegisterComponent},
+  {'path':'login', component:LoginComponent},
 ];
