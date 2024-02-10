ab3503503abe9680028d920f99cc33722663afd5
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-10 15:45:31 -0500

    Add a route for Registration page

--- justTalent/src/app/app.routes.ts
+++ justTalent/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmployerPageComponent } from './employer-page/employer-page.component';
 import { JobDetailsComponent } from './job-details/job-details.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {'path':'', component:HomeComponent},
   {'path':'employer-page/:id', component:EmployerPageComponent},
-  {'path':'job-details/:id', component:JobDetailsComponent}
+  {'path':'job-details/:id', component:JobDetailsComponent},
+  {'path':'register', component:RegisterComponent}
 ];
