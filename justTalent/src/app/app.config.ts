8bc4a257d726ddae597f0c16b66bd5dcd8c2923f
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-10 22:01:28 -0500

    Add auth interceptor

--- justTalent/src/app/app.config.ts
+++ justTalent/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { authInterceptor } from './auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
+  ]
 };
