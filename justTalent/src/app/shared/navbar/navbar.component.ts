ee77e87ae5d560dc1f3c67da9385de5f65bdcfa4
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-10 21:23:15 -0500

    Modify ngOnInit logic

--- justTalent/src/app/shared/navbar/navbar.component.ts
+++ justTalent/src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../interfaces/user';
+import { response } from 'express';
 
 @Component({
   selector: 'app-navbar',
@@ -11,8 +14,14 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent {
   authService = inject(AuthService);
+  http = inject(HttpClient);
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.get<{user: User}>('https://api.realworld.io/api/user').subscribe(response => {
+        console.log('response', response)
+      }
+    )
+  }
 
   logout(): void {
     console.log('logout')
