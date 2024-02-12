65d5dce5ee10f469125a147509fefebb7a18a808
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-12 18:00:42 -0500

    Modify User interface

--- justTalent/src/app/interfaces/user.ts
+++ justTalent/src/app/interfaces/user.ts
@@ -1,5 +1,15 @@
 export interface User {
+  user_id: string;
+  first_name?: string;
+  last_name?: string;
   email: string;
-  token: string;
   username: string;
+  password: string;
+  country?: string;
+  city?: string;
+  resume?: string;
+  formatted_resume?: string;
+  created_at: Date;
+  modified_at: Date;
+  token: string;
 }
