febf4ba169567b6aeb7165e28a5b7fb6f70b639c
Koji Eguchi <koji.eguchi@hotmail.com>
2024-06-09 11:50:08 -0400

    Add conditional rendering for editing resume and populating default values in form

--- justTalent/src/app/candidate/candidate.component.ts
+++ justTalent/src/app/candidate/candidate.component.ts
@@ -4,11 +4,12 @@ import { UserService } from '../services/user.service';
 import { Resume } from '../interfaces/resume';
 import { ResumeService } from '../services/resume.services';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-candidate',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './candidate.component.html',
   styleUrl: './candidate.component.css'
 })
@@ -16,18 +17,65 @@ export class CandidateComponent implements OnInit{
   user: any | undefined;
   resume: Resume | undefined;
 
+  isEditMode:boolean = false;
+  // Non-null assertion operator used here
+  resumeForm!: FormGroup;
+
   constructor(
     private userService: UserService,
-    private resumeService: ResumeService
+    private resumeService: ResumeService,
+    private fb:FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.getCurrentUserDetails();
     this.getCurrentUserResume();
+    this.initializeForm();
+
   }
 
   parseDate(dateString: string): Date | null {
     return dateString ? new Date(dateString) : null;
+  }
+
+  // EDIT BUTTON TOGGLER
+  toggleEditMode(): void {
+    this.isEditMode = !this.isEditMode;
+    if (this.isEditMode && this.resume) {
+      this.populateForm(this.resume); // Update form with resume data
+    }
+  }
+
+  // Populate form with resume data
+  populateForm(resume: Resume): void {
+    this.resumeForm.patchValue({
+      title: resume.title,
+      summary: resume.summary,
+      // Populate other form controls as needed
+    });
+  }
+
+  //FORM SUBMIT HANDLER
+  onSubmit(): void {
+    // Check if the form is valid before proceeding
+    if (this.resumeForm.valid) {
+      // Update the resume object with form values
+      this.resume = {
+        ...this.resume,
+        ...this.resumeForm.value
+      };
+      // Exit edit mode
+      this.toggleEditMode();
+    }
+  }
+
+  // Initialize the form with default values
+  initializeForm(): void {
+    this.resumeForm = this.fb.group({
+      title: [this.resume?.title || ''],
+      summary: [this.resume?.summary || ''],
+      // Add other form controls as needed
+    });
   }
 
   getCurrentUserDetails(): void {
