f12ece0c12c1f0c302408dc0431a8068beff2479
Koji Eguchi <koji.eguchi@hotmail.com>
2024-06-18 18:28:18 -0400

    Create Skills Form component

---
+++ justTalent/src/app/resume-form/skills-form/skills-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills-form.component.html',
+  styleUrl: './skills-form.component.css'
+})
+export class SkillsFormComponent {
+
+}
