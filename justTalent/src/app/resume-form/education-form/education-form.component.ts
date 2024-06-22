27dbd645e6fc9ee122e8b12c1f9fa4b29bed54fb
Koji Eguchi <koji.eguchi@hotmail.com>
2024-06-22 11:11:16 -0400

    Create Education Form component

---
+++ justTalent/src/app/resume-form/education-form/education-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './education-form.component.html',
+  styleUrl: './education-form.component.css'
+})
+export class EducationFormComponent {
+
+}
