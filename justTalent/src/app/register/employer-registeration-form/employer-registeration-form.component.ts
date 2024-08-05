3e7a82c300b76201d535dd0f247379ea0d84d795
Koji Eguchi <koji.eguchi@hotmail.com>
2024-08-05 15:18:09 -0400

    Add employer registration form component

---
+++ justTalent/src/app/register/employer-registeration-form/employer-registeration-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-registeration-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-registeration-form.component.html',
+  styleUrl: './employer-registeration-form.component.css'
+})
+export class EmployerRegisterationFormComponent {
+
+}
