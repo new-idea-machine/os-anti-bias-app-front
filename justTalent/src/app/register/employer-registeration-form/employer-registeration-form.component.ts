2d49bea722ff4883c5fd624d8fb4410cb0778aba
Koji Eguchi <koji.eguchi@hotmail.com>
2024-08-06 19:17:00 -0400

    Add FormBuilder and ReactiveFormModule to the component

--- justTalent/src/app/register/employer-registeration-form/employer-registeration-form.component.ts
+++ justTalent/src/app/register/employer-registeration-form/employer-registeration-form.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employer-registeration-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employer-registeration-form.component.html',
   styleUrl: './employer-registeration-form.component.css'
 })
 export class EmployerRegisterationFormComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+
+  // CREATE EMPLOYER AUTH
+
+  router = inject(Router);
+
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    contact_email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  endpoint = 'http://localhost:3000/api/employers';
+
+  onSubmit(): void {
+    //Store a token to local storage
+    //set a signal/state management
+    //Redirect to a main page
+  }
 
 }
