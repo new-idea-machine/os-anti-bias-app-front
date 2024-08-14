0c94cf6ab5b4e72d5a39bdff67c0497636217235
Koji Eguchi <koji.eguchi@hotmail.com>
2024-08-14 19:06:59 -0400

    Create employer-form component

---
+++ justTalent/src/app/employer-form/employer-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-form.component.html',
+  styleUrl: './employer-form.component.css'
+})
+export class EmployerFormComponent {
+
+}
