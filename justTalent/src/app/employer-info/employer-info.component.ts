dedd34d829192865b73e77a2b55b7fcbeb3865e5
Koji Eguchi <koji.eguchi@hotmail.com>
2024-08-14 20:17:25 -0400

    Add employer-info component for user dashboard preview

---
+++ justTalent/src/app/employer-info/employer-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-info.component.html',
+  styleUrl: './employer-info.component.css'
+})
+export class EmployerInfoComponent {
+
+}
