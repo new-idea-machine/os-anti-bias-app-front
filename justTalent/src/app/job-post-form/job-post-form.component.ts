6c97d6a589756ef4b2349fa6590b9d07fa9d7f26
Koji Eguchi <koji.eguchi@hotmail.com>
2024-09-10 19:42:09 -0400

    Create job-posting-form component

---
+++ justTalent/src/app/job-post-form/job-post-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-post-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-post-form.component.html',
+  styleUrl: './job-post-form.component.css'
+})
+export class JobPostFormComponent {
+
+}
