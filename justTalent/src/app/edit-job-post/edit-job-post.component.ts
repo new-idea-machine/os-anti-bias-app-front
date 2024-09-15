9eb936b809435f0e3e4a429065c57771980ab668
Koji Eguchi <koji.eguchi@hotmail.com>
2024-09-15 15:33:16 -0400

    Create a form to update a job post

--- justTalent/src/app/edit-job-post/edit-job-post.component.ts
+++ justTalent/src/app/edit-job-post/edit-job-post.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JobPost } from '../interfaces/job-post';
+import { JobPostService } from '../services/job-post.service';
 
 @Component({
   selector: 'app-edit-job-post',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-job-post.component.html',
   styleUrl: './edit-job-post.component.css'
 })
-export class EditJobPostComponent {
+export class EditJobPostComponent implements OnInit{
+  @Input() jobPost:JobPost | undefined;
+  jobPostForm: FormGroup;
+
+
+  constructor(
+    private fb:FormBuilder,
+    private jobPostService: JobPostService
+  ){
+    this.jobPostForm = this.fb.group({
+      job_title: ['', Validators.required],
+      requirements: ['', Validators.required],
+      location: ['', Validators.required],
+      type_of_work: ['', Validators.required],
+    });
+  }
+
+
+  ngOnInit(): void {
+    if(this.jobPost) {
+      this.jobPostForm = this.fb.group({
+        job_title: [this.jobPost.job_title, Validators.required],
+        requirements: [this.jobPost.requirements, Validators.required],
+        location: [this.jobPost.location, Validators.required],
+        type_of_work: [this.jobPost.type_of_work, Validators.required],
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.jobPostForm.valid) {
+      console.log('Form data:', this.jobPostForm.value);
+    }
+  }
+
+
+
 
 }
