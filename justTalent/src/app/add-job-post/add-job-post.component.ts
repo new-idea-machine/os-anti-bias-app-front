8cc5b3103cb73ec931d1885035af892c06993934
Koji Eguchi <koji.eguchi@hotmail.com>
2024-09-20 19:30:47 -0400

    Modify input fields for a job posting form

--- justTalent/src/app/add-job-post/add-job-post.component.ts
+++ justTalent/src/app/add-job-post/add-job-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JobPostService } from '../services/job-post.service';
+import { JobPost } from '../interfaces/job-post';
 
 @Component({
   selector: 'app-add-job-post',
@@ -21,6 +22,13 @@ export class AddJobPostComponent implements OnInit {
       requirements: ['', Validators.required],
       location: ['', Validators.required],
       type_of_work: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      type_of_salary: ['', Validators.required],
+      salary: ['', [Validators.required, Validators.min(0)]],
+      description: ['', Validators.required],
+      start_date: ['', Validators.required],
+      end_date: ['', Validators.required],
     });
   }
 
@@ -28,11 +36,26 @@ export class AddJobPostComponent implements OnInit {
 
   }
 
-  cancelEdit():void{
 
+  cancelEdit():void{
+    //Close the form
+    this.jobPostForm.reset();
+  }
+
+  handleFormSubmit(josPost: JobPost): void{
+    this.jobPostService.createJobPost(josPost).subscribe(()=>{
+    // Close the Form
+    this.jobPostForm.reset();
+    });
   }
 
   onSubmit(): void {
-
+    if(this.jobPostForm.valid){
+      const newJobPost = {
+        ...this.jobPostForm.value,
+      }
+      this.handleFormSubmit(newJobPost);
+      // how to re-render a job list?
+    }
   }
 }
