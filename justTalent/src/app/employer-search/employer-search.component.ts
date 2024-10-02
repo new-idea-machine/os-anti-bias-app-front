857e21156e8646e5267766f6a39560b60c5b89eb
Koji Eguchi <koji.eguchi@hotmail.com>
2024-10-01 20:49:20 -0400

    Create employer search component boilerplates

---
+++ justTalent/src/app/employer-search/employer-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-search.component.html',
+  styleUrl: './employer-search.component.css'
+})
+export class EmployerSearchComponent {
+
+}
